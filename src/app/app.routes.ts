f4e0651d65d1f504fd2ebaa0a3be66197a9d09ec
Daniel Rios <wtke90@gmail.com>
2024-02-08 11:25:46 -0500

    Creando componentes e instalando tailwind css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './perfil/login/login.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/login' },
+    { path: 'login', component: LoginComponent },
+];
